30a4187d4bfbe7afa0155eadda0a1c07a85e539d
Himanshu Negi <hnegi@sapient.com>
2019-11-29 16:46:16 +0530

    navigation item added

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './NavigationItems.module.css';
+import NavigationItem from './NavigationItem/NavigationItem';
+const navigationItems = () => {
+  return (
+    <ul className={classes.NavigationItems}>
+      <NavigationItem link='/' active>Burger Builder</NavigationItem>
+      <NavigationItem link='/'>Order Summary</NavigationItem>
+    </ul>
+  )
+}
+
+export default navigationItems;
