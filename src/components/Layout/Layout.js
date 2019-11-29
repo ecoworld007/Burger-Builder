400640326e170c4782cec160770cb7c65d695771
Himanshu Negi <hnegi@sapient.com>
2019-11-30 01:51:02 +0530

    using backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
-const layout = (props) => {
-  return (
-    <Aux>
-      <SideDrawer/>
-      <Toolbar/>
-      <main className={classes.Content}>
-        {props.children}
-      </main>
-    </Aux>
-  );
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
+  closeSideDrawerHandler = () => {
+    this.setState({
+      showSideDrawer: false
+    });
+  }
+  render(){
+    return (
+      <Aux>
+        <SideDrawer closeSideDrawer={this.closeSideDrawerHandler} show={this.state.showSideDrawer}/>
+        <Toolbar/>
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    );
+  }
 }
 
-export default layout;
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,16 +2,26 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
+import Aux from '../../../hoc/Aux';
+import BackDrop from '../../UI/Backdrop/Backdrop';
 const sideDrawer = (props) => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if(props.show){
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
+  console.log(attachedClasses);
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo/>
+    <Aux>
+      <BackDrop clicked={props.closeSideDrawer} show={props.show}/>
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo/>
+        </div>
+        <nav>
+          <NavigationItems/>
+        </nav>
       </div>
-      <nav>
-        <NavigationItems/>
-      </nav>
-    </div>
+    </Aux>
   );
 }
 
